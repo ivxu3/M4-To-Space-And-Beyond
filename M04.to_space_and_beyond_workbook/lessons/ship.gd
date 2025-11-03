9cdfdc8e0e0b170f1f6cf85bd537c8883c34ebf2
Ivan <ivxu3@s.sfusd.edu>
2025-11-03 10:25:39 -0800

    M4L6 WIP

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -7,13 +7,11 @@ var max_speed := normal_speed
 var velocity := Vector2(0, 0)
 var steering_factor := 10.0
 
+
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
-	var desired_velocity := max_speed * direction
-	var steering_vector := desired_velocity - velocity
-	velocity += steering_vector * steering_factor * delta
 
 	if direction.length() > 1.0:
 		direction = direction.normalized()
@@ -22,11 +20,14 @@ func _process(delta: float) -> void:
 		max_speed = boost_speed
 		get_node("Timer").start()
 
-	velocity = direction * max_speed
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
+
 
 func _on_timer_timeout() -> void:
 	max_speed = normal_speed
